002f2c2b930af21ac80d180fc506b81aa6fd5ecb
Matthew Barbour <sanmadjack@gmail.com>
2014-05-13 15:41:34 -0500

    Initial commit, basic juicebox support

---
+++ JuiceBoxGalleryDownloader/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace JuiceBoxGalleryDownloader {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.urlsTxt = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.filesProg = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dirTxt = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.urlsProg = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // urlsTxt
+            // 
+            this.urlsTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.urlsTxt.Location = new System.Drawing.Point(3, 16);
+            this.urlsTxt.Multiline = true;
+            this.urlsTxt.Name = "urlsTxt";
+            this.urlsTxt.Size = new System.Drawing.Size(462, 69);
+            this.urlsTxt.TabIndex = 0;
+            this.urlsTxt.TextChanged += new System.EventHandler(this.urlsTxt_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.urlsTxt);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(468, 88);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gallery URL(s)";
+            // 
+            // goBtn
+            // 
+            this.goBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.goBtn.Enabled = false;
+            this.goBtn.Location = new System.Drawing.Point(12, 164);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(468, 23);
+            this.goBtn.TabIndex = 2;
+            this.goBtn.Text = "Go!";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTxt.Location = new System.Drawing.Point(12, 193);
+            this.outputTxt.Multiline = true;
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.ReadOnly = true;
+            this.outputTxt.Size = new System.Drawing.Size(468, 133);
+            this.outputTxt.TabIndex = 3;
+            // 
+            // filesProg
+            // 
+            this.filesProg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filesProg.Location = new System.Drawing.Point(12, 361);
+            this.filesProg.Name = "filesProg";
+            this.filesProg.Size = new System.Drawing.Size(468, 23);
+            this.filesProg.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.dirTxt);
+            this.groupBox2.Location = new System.Drawing.Point(12, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(468, 51);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Download Folder";
+            // 
+            // dirTxt
+            // 
+            this.dirTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dirTxt.Location = new System.Drawing.Point(7, 20);
+            this.dirTxt.Name = "dirTxt";
+            this.dirTxt.ReadOnly = true;
+            this.dirTxt.Size = new System.Drawing.Size(374, 20);
+            this.dirTxt.TabIndex = 0;
+            this.dirTxt.TextChanged += new System.EventHandler(this.dirTxt_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(387, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Select Donwload Folder";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // urlsProg
+            // 
+            this.urlsProg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlsProg.Location = new System.Drawing.Point(12, 332);
+            this.urlsProg.Name = "urlsProg";
+            this.urlsProg.Size = new System.Drawing.Size(468, 23);
+            this.urlsProg.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 396);
+            this.Controls.Add(this.urlsProg);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.filesProg);
+            this.Controls.Add(this.outputTxt);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(500, 350);
+            this.Name = "Form1";
+            this.Text = "JuiceBoxGalleryDownloader by Matthew Barbour";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlsTxt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.TextBox outputTxt;
+        private System.Windows.Forms.ProgressBar filesProg;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox dirTxt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar urlsProg;
+    }
+}
+
---
+++ JuiceBoxGalleryDownloader/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace JuiceBoxGalleryDownloader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            if(!string.IsNullOrWhiteSpace(Properties.Settings.Default.DownloadFolder)) {
+                if (!Directory.Exists(Properties.Settings.Default.DownloadFolder)) {
+                    Properties.Settings.Default.DownloadFolder = "";
+                    Properties.Settings.Default.Save();
+                } else {
+                    this.dirTxt.Text = Properties.Settings.Default.DownloadFolder;
+                }
+            }
+
+
+        }
+
+        private void checkEnableds() {
+            goBtn.Enabled = !string.IsNullOrWhiteSpace(dirTxt.Text) && ! string.IsNullOrWhiteSpace(urlsTxt.Text);
+        }
+
+        private void dirTxt_TextChanged(object sender, EventArgs e) {
+            checkEnableds();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.folderBrowserDialog1.SelectedPath = dirTxt.Text;
+            if (this.folderBrowserDialog1.ShowDialog(this) == System.Windows.Forms.DialogResult.OK) {
+                dirTxt.Text = this.folderBrowserDialog1.SelectedPath;
+                Properties.Settings.Default.DownloadFolder = dirTxt.Text;
+                Properties.Settings.Default.Save();
+            } else {
+
+            }
+        }
+
+        private void urlsTxt_TextChanged(object sender, EventArgs e) {
+            checkEnableds();
+        }
+
+
+
+        private void goBtn_Click(object sender, EventArgs e) {
+            try {
+                goBtn.Enabled = false;
+                button2.Enabled = false;
+                urlsTxt.Enabled = false;
+                outputTxt.Text = "";
+                backgroundWorker1.RunWorkerAsync();
+            } catch (Exception ex) {
+                MessageBox.Show(this, ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
+            List<String> urls = new List<string>();
+            urls.AddRange(urlsTxt.Text.Split('\n'));
+
+            for(int i = 0; i < urls.Count; i++ ) {
+                string url = urls[i];
+                backgroundWorker1.ReportProgress((i/urls.Count)*100,"urls: Processing URL " + url);
+
+                using (WebClient wc = new WebClient()) {
+                    JuiceBoxConfigFile config_file;
+                    using (MemoryStream stream = new MemoryStream(wc.DownloadData(url))) {
+                        config_file = new JuiceBoxConfigFile(stream);
+                    }
+                    List<String> images = config_file.GetImageURLS();
+                    for(int j = 0; j < images.Count; j++) {
+                        string image = images[j];
+                        Uri uri = new Uri(image);
+                        //if (uri.IsFile) {
+                            string filename = System.IO.Path.GetFileName(uri.LocalPath);
+                            wc.DownloadFile(image, Path.Combine(dirTxt.Text, filename));
+                            backgroundWorker1.ReportProgress((j / images.Count) * 100, "images: Downloading Image" + image);
+                        //}
+                    }
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            string text = e.UserState.ToString();
+            if (text.StartsWith("urls: ")) {
+                filesProg.Value = e.ProgressPercentage;
+                text = text.Substring(6);
+            } else if (text.StartsWith("images: ")) {
+                urlsProg.Value = e.ProgressPercentage;
+                text = text.Substring(8);
+            }
+            
+                outputTxt.Text += e.UserState.ToString() + Environment.NewLine;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            if (e.Error != null) {
+                MessageBox.Show(this, e.Error.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                outputTxt.Text += e.Error.Message + Environment.NewLine;
+            }
+            MessageBox.Show(this, "Done!");
+            goBtn.Enabled = true;
+            button2.Enabled = true;
+            urlsTxt.Enabled = true;
+        }
+    }
+}
